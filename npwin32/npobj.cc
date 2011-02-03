79d2935208e3b60b5018817d674391538e6752d6
Yosuke HASEGAWA <hasegawa@utf-8.jp>
2011-02-03 17:22:59 +0900

    fix memory leaks

--- npwin32/npobj.cc
+++ npwin32/npobj.cc
@@ -1,4 +1,4 @@
-#include	"npobj.h"
+#include    "npobj.h"
 
 #ifdef __cplusplus
 extern "C" {
@@ -11,100 +11,121 @@ extern NPNetscapeFuncs *npnfuncs;
 
 
 struct NPClass NPObj::_npo_class = {
-	NP_CLASS_STRUCT_VERSION,
-	NULL,
-	NULL,
-	NULL,
-	_hasMethod,
-	_invoke,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
-	NULL,
+    NP_CLASS_STRUCT_VERSION,
+    NULL,//_allocate,
+    _deallocate,
+    NULL,
+    _hasMethod,
+    _invoke,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
+    NULL,
 };
 
-NPObj::NPObj( NPP instance ) 
+NPObj::NPObj( NPP instance, bool retainObject ) 
 {
-	LOGF;
-	_npobject = npnfuncs->createobject( instance, &_npo_class );
-	npnfuncs->retainobject( _npobject );
-	_npp = instance;
-	NPObj::_map[ _npobject ] = this;
+    LOGF;
+    _npobject = npnfuncs->createobject( instance, &_npo_class );
+    if( retainObject ){
+        npnfuncs->retainobject( _npobject );
+    }
+    _npp = instance;
+    NPObj::_map[ _npobject ] = this;
 }
 
 NPObj::~NPObj()
 {
-	LOGF;
-	NPObj::_map.RemoveKey( _npobject );
-	npnfuncs->releaseobject( _npobject );
+    LOGF;
+    NPObj::_map.RemoveKey( _npobject );
+    npnfuncs->releaseobject( _npobject );
 }
 
 bool NPObj::_hasMethod( NPObject *obj, NPIdentifier methodName )
 {
-	bool r;
-	DWORD w;
-	LPWSTR s;
-	NPObj* npobj;
-	NPUTF8 *name = npnfuncs->utf8fromidentifier( methodName );
+    bool r;
+    DWORD w;
+    LPWSTR s;
+    NPObj* npobj;
+    NPUTF8 *name = npnfuncs->utf8fromidentifier( methodName );
 
-	LOGF;
-	if( !NPObj::_map.Lookup( obj, npobj ) ){
-		return false;
-	}
-	w = MultiByteToWideChar( CP_UTF8, 0, name, -1, NULL, 0 );
-	s = new WCHAR[ w + 1 ];
-	MultiByteToWideChar( CP_UTF8, 0, name, -1, s, w );
-	LOG( L"methodName=%s", s );
-	r = npobj->hasMethod( s );
+    LOGF;
+    if( !NPObj::_map.Lookup( obj, npobj ) ){
+        return false;
+    }
+    w = MultiByteToWideChar( CP_UTF8, 0, name, -1, NULL, 0 );
+    s = new WCHAR[ w + 1 ];
+    MultiByteToWideChar( CP_UTF8, 0, name, -1, s, w );
+    LOG( L"methodName=%s", s );
+    r = npobj->hasMethod( s );
 
-	delete s;
-	npnfuncs->memfree( name );
-	return r;
+    delete s;
+    npnfuncs->memfree( name );
+    return r;
 
 }
 
 bool NPObj::_invoke( NPObject *obj, NPIdentifier methodName, 
-		const NPVariant *args, uint32_t argCount, NPVariant *result) 
+        const NPVariant *args, uint32_t argCount, NPVariant *result) 
 {
-	bool r;
-	DWORD w;
-	LPWSTR s;
-	NPObj* npobj;
-	NPUTF8 *name = npnfuncs->utf8fromidentifier( methodName );
+    bool r;
+    DWORD w;
+    LPWSTR s;
+    NPObj* npobj;
+    NPUTF8 *name = npnfuncs->utf8fromidentifier( methodName );
 
-	LOGF;
-	if( !NPObj::_map.Lookup( obj, npobj ) ){
-		return false;
-	}
-	w = MultiByteToWideChar( CP_UTF8, 0, name, -1, NULL, 0 );
-	s = new WCHAR[ w + 1 ];
-	MultiByteToWideChar( CP_UTF8, 0, name, -1, s, w );
-	LOG( L"methodName=%s", s );
-	r = npobj->invoke( s, args, argCount, result );
-	delete s;
-	npnfuncs->memfree( name );
-	return r;
+    LOGF;
+    if( !NPObj::_map.Lookup( obj, npobj ) ){
+        return false;
+    }
+    w = MultiByteToWideChar( CP_UTF8, 0, name, -1, NULL, 0 );
+    s = new WCHAR[ w + 1 ];
+    MultiByteToWideChar( CP_UTF8, 0, name, -1, s, w );
+    LOG( L"methodName=%s", s );
+    r = npobj->invoke( s, args, argCount, result );
+    delete s;
+    npnfuncs->memfree( name );
+    return r;
+}
+
+/* 
+NPObject* NPObj::_allocate( NPP npp, NPClass *aClass )
+{
+    NPObj* p;
+
+    LOGF;
+    p = new NPObj( npp );
+    return p->getNPObject();
+}
+*/
+
+void NPObj::_deallocate( NPObject *obj )
+{
+    NPObj *npobj;
+
+    LOGF;
+
+    if( !NPObj::_map.Lookup( obj, npobj ) ) return;
+    delete npobj;
 }
 
 NPObject* NPObj::getNPObject()
 {
-	LOGF;
-	return _npobject;
+    return _npobject;
 }
 
 NPP NPObj::getNPP()
 {
-	LOGF;
-	return _npp;
+    return _npp;
 }
 
 #pragma warning( push )
 #pragma warning( disable : 4100 )
 bool NPObj::hasMethod( LPCWSTR methodName )
 {
-	LOGF;
-	return false;
+    LOGF;
+    return false;
 }
 #pragma warning( pop )
 
@@ -112,8 +133,8 @@ bool NPObj::hasMethod( LPCWSTR methodName )
 #pragma warning( disable : 4100 )
 bool NPObj::invoke( LPCWSTR methodName, const NPVariant *args, uint32_t argCount, NPVariant *result) 
 {
-	LOGF;
-	return false;
+    LOGF;
+    return false;
 }
 #pragma warning( pop )
 
@@ -125,110 +146,113 @@ LPCSTR NPOE_GENERIC_ERROR = "error";
 
 LPCSTR checkNpArgs( LPCSTR lpszCheck, const NPVariant *args, uint32_t argCount )
 {
-	DWORD i;
-	LOGF;
-	LOG( "argCount=%d", argCount );
-	for( i = 0; i < argCount; i++ ){
-		if( !*lpszCheck ) break;
-		switch( *lpszCheck ){
-		case 'V' : // VOID
-			if( !NPVARIANT_IS_VOID( args[ i ] ) ) return NPOE_INVALID_ARG_TYPE;
-			break;
-		case 'N' : // NULL
-			if( !NPVARIANT_IS_NULL( args[ i ] ) ) return NPOE_INVALID_ARG_TYPE;
-			break;
-		case 'B' : // boolean
-			if( !NPVARIANT_IS_BOOLEAN( args[ i ] ) ) return NPOE_INVALID_ARG_TYPE;
-			break;
-		case 'I' : // INT32
-			if( !NPVARIANT_IS_INT32( args[ i ] ) ) return NPOE_INVALID_ARG_TYPE;
-			break;
-		case 'D' : // DOUBLE
-			if( !NPVARIANT_IS_DOUBLE( args[ i ] ) ) return NPOE_INVALID_ARG_TYPE;
-			break;
-		case 'S' : // STRING
-			if( !NPVARIANT_IS_STRING( args[ i ] ) ) return NPOE_INVALID_ARG_TYPE;
-			break;
-		case 'O' : // OBJECT
-			if( !NPVARIANT_IS_OBJECT( args[ i ] ) ) return NPOE_INVALID_ARG_TYPE;
-			break;
-		case '*' : // anything
-		case '?':
-			break;
-		default:
-			return NPOE_GENERIC_ERROR;
-		}
-		*lpszCheck++;
-	}
-	if( *lpszCheck ) return NPOE_MISSING_ARGUMENT;
-	return NULL;
+    DWORD i;
+    LOGF;
+    LOG( "argCount=%d", argCount );
+    for( i = 0; i < argCount; i++ ){
+        if( !*lpszCheck ) break;
+        switch( *lpszCheck ){
+        case 'V' : // VOID
+            if( !NPVARIANT_IS_VOID( args[ i ] ) ) return NPOE_INVALID_ARG_TYPE;
+            break;
+        case 'N' : // NULL
+            if( !NPVARIANT_IS_NULL( args[ i ] ) ) return NPOE_INVALID_ARG_TYPE;
+            break;
+        case 'B' : // boolean
+            if( !NPVARIANT_IS_BOOLEAN( args[ i ] ) ) return NPOE_INVALID_ARG_TYPE;
+            break;
+        case 'I' : // INT32
+            if( !NPVARIANT_IS_INT32( args[ i ] ) ) return NPOE_INVALID_ARG_TYPE;
+            break;
+        case 'D' : // DOUBLE
+            if( !NPVARIANT_IS_DOUBLE( args[ i ] ) ) return NPOE_INVALID_ARG_TYPE;
+            break;
+        case 'S' : // STRING
+            if( !NPVARIANT_IS_STRING( args[ i ] ) ) return NPOE_INVALID_ARG_TYPE;
+            break;
+        case 'O' : // OBJECT
+            if( !NPVARIANT_IS_OBJECT( args[ i ] ) ) return NPOE_INVALID_ARG_TYPE;
+            break;
+        case '*' : // anything
+        case '?':
+            break;
+        default:
+            return NPOE_GENERIC_ERROR;
+        }
+        *lpszCheck++;
+    }
+    if( *lpszCheck ) return NPOE_MISSING_ARGUMENT;
+    return NULL;
 }
 
 LPWSTR Npv2WStr( NPVariant v )
 {
-	DWORD w;
-	LPWSTR s;
+    DWORD w;
+    LPWSTR s;
 
-	if( NPVARIANT_IS_NULL( v ) ){
-		s = new WCHAR[ 1 ];
-		*s = L'\0';
-	}else if( NPVARIANT_IS_BOOLEAN( v ) ){
-		s = new WCHAR[ 6 ];
-		if( NPVARIANT_TO_BOOLEAN( v ) ){
-			StringCchCopyW( s, 6, L"true" );
-		}else{
-			StringCchCopyW( s, 6, L"false" );
-		}
-	}else if( NPVARIANT_IS_INT32( v ) ){
-		s = new WCHAR[ 11 ];
-		StringCchPrintfW( s, 11, L"%d", NPVARIANT_TO_INT32( v ) );
-	}else if( NPVARIANT_IS_DOUBLE( v ) ){
-		s = new WCHAR[ 30 ];
-		StringCchPrintfW( s, 11, L"%f", NPVARIANT_TO_DOUBLE( v ) );
-	}else if( NPVARIANT_IS_STRING( v ) ){
-		NPString str = NPVARIANT_TO_STRING( v );
-		w = MultiByteToWideChar( CP_UTF8, 0, str.UTF8Characters, str.UTF8Length + 1, NULL, 0 );
-		s = new WCHAR[ w + 1 ];
-		MultiByteToWideChar( CP_UTF8, 0, str.UTF8Characters, str.UTF8Length + 1, s, w );
-	}
-	return s;
+    if( NPVARIANT_IS_NULL( v ) ){
+        s = new WCHAR[ 1 ];
+        *s = L'\0';
+    }else if( NPVARIANT_IS_BOOLEAN( v ) ){
+        s = new WCHAR[ 6 ];
+        if( NPVARIANT_TO_BOOLEAN( v ) ){
+            StringCchCopyW( s, 6, L"true" );
+        }else{
+            StringCchCopyW( s, 6, L"false" );
+        }
+    }else if( NPVARIANT_IS_INT32( v ) ){
+        s = new WCHAR[ 11 ];
+        StringCchPrintfW( s, 11, L"%d", NPVARIANT_TO_INT32( v ) );
+    }else if( NPVARIANT_IS_DOUBLE( v ) ){
+        s = new WCHAR[ 30 ];
+        StringCchPrintfW( s, 11, L"%f", NPVARIANT_TO_DOUBLE( v ) );
+    }else if( NPVARIANT_IS_STRING( v ) ){
+        NPString str = NPVARIANT_TO_STRING( v );
+        w = MultiByteToWideChar( CP_UTF8, 0, str.UTF8Characters, str.UTF8Length + 1, NULL, 0 );
+        s = new WCHAR[ w + 1 ];
+        MultiByteToWideChar( CP_UTF8, 0, str.UTF8Characters, str.UTF8Length + 1, s, w );
+    }else{
+        s = new WCHAR[ 1 ];
+        *s = L'\0';
+    }
+    return s;
 }
 
 LPSTR Npv2Str( NPVariant v )
 {
-	DWORD w;
-	LPSTR s;
-	LPWSTR ws;
+    DWORD w;
+    LPSTR s;
+    LPWSTR ws;
 
-	ws = Npv2WStr( v );
-	if( ws == NULL ) return NULL;
+    ws = Npv2WStr( v );
+    if( ws == NULL ) return NULL;
 
-	w = WideCharToMultiByte( CP_ACP, 0, ws, -1, NULL, 0, NULL, NULL );
-	s = new CHAR[ w + 1 ];
-	WideCharToMultiByte( CP_ACP, 0, ws, -1, s, w, NULL, NULL );
-	delete ws;
-	return s;
+    w = WideCharToMultiByte( CP_ACP, 0, ws, -1, NULL, 0, NULL, NULL );
+    s = new CHAR[ w + 1 ];
+    WideCharToMultiByte( CP_ACP, 0, ws, -1, s, w, NULL, NULL );
+    delete ws;
+    return s;
 }
 
 int Npv2Int( NPVariant v )
 {
-	if( NPVARIANT_IS_NULL( v ) ){
-		return 0;
-	}else if( NPVARIANT_IS_BOOLEAN( v ) ){
-		if( NPVARIANT_TO_BOOLEAN( v ) ){
-			return 1;
-		}else{
-			return 0;
-		}
-	}else if( NPVARIANT_IS_INT32( v ) ){
-		return NPVARIANT_TO_INT32( v );
-	}else if( NPVARIANT_IS_STRING( v ) ){
-		int n;
-		LPCWSTR s = Npv2WStr( v );
-		if( StrToIntExW( s, STIF_DEFAULT, &n ) ){
-			return n;
-		}
-	}
-	return 0;
+    if( NPVARIANT_IS_NULL( v ) ){
+        return 0;
+    }else if( NPVARIANT_IS_BOOLEAN( v ) ){
+        if( NPVARIANT_TO_BOOLEAN( v ) ){
+            return 1;
+        }else{
+            return 0;
+        }
+    }else if( NPVARIANT_IS_INT32( v ) ){
+        return NPVARIANT_TO_INT32( v );
+    }else if( NPVARIANT_IS_STRING( v ) ){
+        int n;
+        LPCWSTR s = Npv2WStr( v );
+        if( StrToIntExW( s, STIF_DEFAULT, &n ) ){
+            return n;
+        }
+    }
+    return 0;
 }
 
--- npwin32/npobj.h
+++ npwin32/npobj.h
@@ -1,64 +1,83 @@
 #pragma once
 
-#include	<windows.h>
-#include	<atlstr.h>
-#include	<atlcoll.h>
-#include	<strsafe.h>
+#include    <windows.h>
+#include    <atlstr.h>
+#include    <atlcoll.h>
+#include    <strsafe.h>
 #include <npapi.h>
 #include <npfunctions.h>
 #include <npruntime.h>
 
 class NPObj : public NPObject {
 private:
-	static void *p;
-	static CAtlMap<NPObject*, NPObj*> _map;
-	static bool _hasMethod( NPObject*, NPIdentifier );
-	static bool _invoke( NPObject*, NPIdentifier, const NPVariant*, uint32_t, NPVariant*); 
+    static void *p;
+    static CAtlMap<NPObject*, NPObj*> _map;
+    // static NPObject* _allocate( NPP, NPClass * );
+    static void _deallocate( NPObject * );
+    static bool _hasMethod( NPObject*, NPIdentifier );
+    static bool _invoke( NPObject*, NPIdentifier, const NPVariant*, uint32_t, NPVariant* ); 
 public:
-	NPObj( NPP );
-	~NPObj();
-	NPObject* getNPObject();
-	NPP getNPP();
-	static struct NPClass _npo_class;
-	virtual bool hasMethod( LPCWSTR methodName );
-	virtual bool invoke( LPCWSTR methodName, const NPVariant *args, uint32_t argCount, NPVariant *result);
+    NPObj( NPP, bool );
+    virtual ~NPObj();
+    NPObject* getNPObject();
+    NPP getNPP();
+    static struct NPClass _npo_class;
+    virtual bool hasMethod( LPCWSTR methodName );
+    virtual bool invoke( LPCWSTR methodName, const NPVariant *args, uint32_t argCount, NPVariant *result);
 protected:
-	NPObject* _npobject;
-	NPP _npp;
+    NPObject* _npobject;
+    NPP _npp;
 };
 
 
 inline void LOG( LPCSTR lpszFormat, ... )
 {
 #ifdef DEBUG
-	CHAR buf[ 4096 ];
-	va_list ap;
+    CHAR buf[ 4096 ];
+    va_list ap;
 
-	va_start( ap, lpszFormat );
-	StringCchVPrintfA( buf, _countof( buf ), lpszFormat, ap );
-	OutputDebugStringA( buf );
+    va_start( ap, lpszFormat );
+    StringCchVPrintfA( buf, _countof( buf ), lpszFormat, ap );
+    OutputDebugStringA( buf );
 #endif
 }
 
 inline void LOG( LPCWSTR lpszFormat, ... )
 {
 #ifdef DEBUG
-	WCHAR buf[ 4096 ];
-	va_list ap;
+    WCHAR buf[ 4096 ];
+    va_list ap;
 
-	va_start( ap, lpszFormat );
-	StringCchVPrintfW( buf, _countof( buf ), lpszFormat, ap );
-	OutputDebugStringW( buf );
+    va_start( ap, lpszFormat );
+    StringCchVPrintfW( buf, _countof( buf ), lpszFormat, ap );
+    OutputDebugStringW( buf );
 #endif
 }
+
+#ifdef DEBUG
+class _DEBUG_FUNC_INFO{
+private:
+    const char *p;
+public:
+    _DEBUG_FUNC_INFO( const char*funcname, int line )
+    {
+        p = funcname;
+        LOG( "*Enter to %s (%d)", p, line );
+    };
+    ~_DEBUG_FUNC_INFO()
+    {
+        LOG( "*Leave from %s", p );
+    };
+};
+
+//#d efine LOGF        LOG( "Enter %s (%d)", __FUNCTION__, __LINE__ )
+#define LOGF    _DEBUG_FUNC_INFO    _debug_func_info( __FUNCTION__, __LINE__ )
+#else
+#define LOGF
+#endif
 
 LPCSTR checkNpArgs( LPCSTR lpszCheck, const NPVariant *args, uint32_t argCount );
 LPWSTR Npv2WStr( NPVariant v );
 LPSTR Npv2Str( NPVariant v );
 int Npv2Int( NPVariant v );
 
-#ifdef DEBUG
-#define	LOGF		LOG( "Enter %s (%d)", __FUNCTION__, __LINE__ )
-#else
-#define	LOGF
-#endif
